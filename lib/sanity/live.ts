c0590674e379b2e3b95d148fd34c1b0ae8711c95
Sultan <ahmad.sultan.2777@gmail.com>
2026-06-18 21:09:10 +0700

    fix: updated env value

--- lib/sanity/live.ts
+++ lib/sanity/live.ts
@@ -1,9 +1,9 @@
 import { defineLive } from "next-sanity/live";
 import { client } from "./client";
 
-const token = process.env.NEXT_PUBLIC_SANITY_API_READ_TOKEN;
+const token = process.env.NEXT_PUBLIC_SANITY_TOKEN;
 if (!token) {
-  throw new Error("Missing NEXT_PUBLIC_SANITY_API_READ_TOKEN");
+  throw new Error("Missing NEXT_PUBLIC_SANITY_TOKEN");
 }
 
 export const { sanityFetch, SanityLive } = defineLive({
